c19cbdea67c524f04a1125a04d8d4745f79f1d78
Dmitriev Andrey <mohnatishmel@gmail.com>
2021-01-08 12:16:19 +0300

    Add clone method to Convertible interface

--- src/main/java/by/itacademy/taskcore/model/Convertible.java
+++ src/main/java/by/itacademy/taskcore/model/Convertible.java
@@ -2,6 +2,8 @@ package by.itacademy.taskcore.model;
 
 public interface Convertible extends MeasurementContainer{
 
+    Convertible clone();
+
     void setValue(double value);
 
     void setUnit(String unit);
