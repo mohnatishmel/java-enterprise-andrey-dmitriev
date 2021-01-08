5f6fe5aa5ed13076b7fcb00eff4b1e709c43abb8
Dmitriev Andrey <mohnatishmel@gmail.com>
2021-01-08 13:52:47 +0300

    Add DaoException.java

---
+++ src/main/java/by/itacademy/taskcore/exception/DaoException.java
@@ -0,0 +1,8 @@
+package by.itacademy.taskcore.exception;
+
+public class DaoException extends Exception {
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
