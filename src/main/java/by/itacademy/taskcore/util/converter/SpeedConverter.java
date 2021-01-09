699706c7d66c24eb7ef42bfe97b93130c69dc11a
Dmitriev Andrey <mohnatishmel@gmail.com>
2021-01-09 16:10:10 +0300

    SpeedConverter methods non static Delete IllegalUnitTypeException.java

--- src/main/java/by/itacademy/taskcore/util/converter/SpeedConverter.java
+++ src/main/java/by/itacademy/taskcore/util/converter/SpeedConverter.java
@@ -1,51 +1,60 @@
 package by.itacademy.taskcore.util.converter;
 
-import by.itacademy.taskcore.exception.IllegalUnitTypeException;
-import by.itacademy.taskcore.model.Convertible;
+import by.itacademy.taskcore.model.Measurement;
+import by.itacademy.taskcore.model.MeasurementContainer;
+import by.itacademy.taskcore.model.speed.Speed;
+
+import java.util.List;
 
 public class SpeedConverter {
 
-    public static Convertible convertToMetersPerSecond(Convertible convertible) throws IllegalUnitTypeException {
-        double value = convertible.getValue();
-        String unit = convertible.getUnit();
+    public List<MeasurementContainer<Speed>> convertToMetersPerSecond(List<MeasurementContainer<Speed>> list) {
+        for (MeasurementContainer<Speed> mc : list) {
+            Measurement measurement = mc.getOriginal();
+            convertToMetersPerSecond(measurement);
+            mc.add(measurement);
+        }
+        return list;
+    }
+
+    public Measurement convertToMetersPerSecond(Measurement measurement) {
+        double value = measurement.getValue();
+        String unit = measurement.getUnit();
         if (unit != null) {
-            if (unit.equals("kmh")) {
+            if (unit.equals("ms")) {
+                return measurement;
+            } if (unit.equals("kmh")) {
                 value = kilometersPerHourToMetersPerSecond(value);
             }  if (unit.equals("mph")) {
                 value = milesPerHourToMetersPerSecond(value);
             }  if (unit.equals("kn")) {
                 value = knotsToMetersPerSecond(value);
-            } else {
-                throw new IllegalUnitTypeException("Provided unit type is not supported");
             }
-        } else {
-            throw new IllegalUnitTypeException("Unit type can't be null");
         }
-        convertible = convertible.clone();
-        convertible.setUnit("ms");
-        convertible.setValue(value);
-        return convertible;
+        measurement.setUnit("ms");
+        measurement.setValue(value);
+        return measurement;
     }
 
-    private static double kilometersPerHourToMetersPerSecond(double value) {
+    private double kilometersPerHourToMetersPerSecond(double value) {
         value *= 1000;
         value = metersPerHourToMetersPerSecond(value);
         return value;
     }
 
-    private static double milesPerHourToMetersPerSecond(double value) {
+    private double milesPerHourToMetersPerSecond(double value) {
         value *= 1609;
         value = metersPerHourToMetersPerSecond(value);
         return value;
     }
 
-    private static double knotsToMetersPerSecond(double value) {
+    private double knotsToMetersPerSecond(double value) {
         value *= 1852;
         value = metersPerHourToMetersPerSecond(value);
         return value;
     }
 
-    private static double metersPerHourToMetersPerSecond(double value) {
+    private double metersPerHourToMetersPerSecond(double value) {
         return value / 3600;
     }
 }
