9a74929bc2fdc9afa9d528627a795683e35d9a2f
Dmitriev Andrey <mohnatishmel@gmail.com>
2021-02-09 10:31:51 +0300

    Refactoring functionality to abstract classes

---
+++ src/main/java/by/itacademy/dal/jdbc/query/user/RoleJdbcSqlQueryHolder.java
@@ -0,0 +1,17 @@
+package by.itacademy.dal.jdbc.query.user;
+
+public class RoleJdbcSqlQueryHolder {
+
+    private static final String GET_BY_ID_SQL = "SELECT role_id, role  FROM roles WHERE role_id = ?;";
+    private static final String GET_BY_ROLE_NAME_SQL = "SELECT role_id, role  FROM roles WHERE role = ?;";
+
+    public String getByIdSql() {
+        return GET_BY_ID_SQL;
+    }
+
+    public String getGetByNameSql() {
+        return GET_BY_ROLE_NAME_SQL;
+    }
+
+    }
+}
---
+++ src/main/java/by/itacademy/dal/jdbc/statement/user/PersonalInformationStatementInitializer.java
@@ -0,0 +1,22 @@
+package by.itacademy.dal.jdbc.statement.user;
+
+import by.itacademy.dal.jdbc.statement.StatementInitializer;
+import by.itacademy.model.user.PersonalInformation;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class PersonalInformationStatementInitializer  implements StatementInitializer<PersonalInformation> {
+
+    @Override
+    public void processUpdateStatement(PreparedStatement stmt, PersonalInformation personalInformation) throws SQLException {
+        processCreateStatement(stmt, personalInformation);
+        stmt.setInt(3, personalInformation.getId());
+    }
+
+    @Override
+    public void processCreateStatement(PreparedStatement stmt, PersonalInformation personalInformation) throws SQLException {
+        stmt.setString(1, personalInformation.getFirstName());
+        stmt.setString(2, personalInformation.getLastName());
+    }
+}
